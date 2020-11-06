ad9cfda9d02b81b8b32c1b5d508cddb7f1000038
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-11-06 22:41:53 +0000

    first push

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "delete the users"
+User.destroy_all
+puts "create user"
+user = User.create!(email: "user@user.com",password:"123456")
+puts "user created"
+
+channels = [general,coding_buddies , react, rails]
+
+puts "channels"
+Channel.destroy_all
+puts "create channels"
+user = Channel.create!(email: "user@user.com",password:"123456")
+puts "user created"
+
