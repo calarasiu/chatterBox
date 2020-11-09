4d750dba629f4ba2ac750d8225f01c50a0371af1
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-11-09 09:37:01 +0000

    add create message method

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,16 +1,21 @@
 class Api::V1::MessagesController < ApplicationController
-  before_action :find_channel
+  before_action :set_channel
+
   def index
     messages = @channel.messages.order('created_at ASC')
     render json: messages # see Message.as_json method
   end
 
   def create
+    message = @channel.messages.build(content: params[:content])
+    message.user = current_user
+    message.save
+    render json: message # see Message.as_json method
   end
 
   private
 
-  def find_channel
+  def set_channel
     @channel = Channel.find_by(name: params[:channel_id])
   end
 end
